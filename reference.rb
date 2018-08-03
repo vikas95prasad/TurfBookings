eaf5bd01a9f8cf23117d752ee48e4f0fef293e74
vikas95prasad <vikaskumar@amuratech.com>
2018-08-04 00:47:23 +0530

    add user with role

--- reference.rb
+++ reference.rb
@@ -1,13 +1,15 @@
 #Model Relations
-class Restaurant < ApplicationRecord
- has_many :bookings
- has_many :users, through: :bookings
-end
-class Booking < ApplicationRecord
- belongs_to :restaurant
- belongs_to :user
-end
-class User < ApplicationRecord
- has_many :bookings
- has_many :restaurants, through: :bookings
-end
+# class Restaurant < ApplicationRecord
+#  has_many :bookings
+#  has_many :users, through: :bookings
+# end
+# class Booking < ApplicationRecord
+#  belongs_to :restaurant
+#  belongs_to :user
+# end
+# class User < ApplicationRecord
+#  has_many :bookings
+#  has_many :restaurants, through: :bookings
+# end
+
+
